0fba2c28e08a6e07f942f756fdd1a672182a5f5f
bankovskiy <it.impostor@gmail.com>
2023-11-18 16:26:06 +0600

    task09 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var remove = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        savedInstanceState?.let { if(it.getBoolean("removed")) binding.rootLayout.removeView(binding.titleTextView) }
+        binding.removeButton.setOnClickListener {
+            remove = !remove
+            binding.rootLayout.removeView(binding.titleTextView) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("removed", remove)
     }
 }

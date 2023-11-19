ff21f4c132cf334c856d8988c12a8b2245961dc1
bankovskiy <it.impostor@gmail.com>
2023-11-19 21:27:40 +0600

    task11 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        savedInstanceState?.let {
+            counter = it.getString("counter")?.toInt() ?: 0
+            binding.countTextView.text = it.getString("counter") }
+        binding.incrementButton.setOnClickListener {
+            counter += 2
+            binding.countTextView.text = counter.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("counter", counter.toString())
     }
 }
